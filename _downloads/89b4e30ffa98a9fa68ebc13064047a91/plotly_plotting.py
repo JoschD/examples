7438ebad464897175ccaab436f1c8151e4ce433a
JoschD <26184899+JoschD@users.noreply.github.com>
2022-04-11 08:40:39 +0000

    Deploying to gh-pages from  @ c4fcebaf8d5c04b9dc9649d3006b1a61f10ab6c2 🚀

--- _downloads/89b4e30ffa98a9fa68ebc13064047a91/plotly_plotting.py
+++ _downloads/89b4e30ffa98a9fa68ebc13064047a91/plotly_plotting.py
@@ -20,13 +20,9 @@ fig.update_layout(height=400)
 fig
 
 # %%
-#
-# .. tip::
-#       Some Tip about how to use this.     
-#       Also, a reference, because why not :cite:`TomasAmplitudeDependentClosest2016`.
-
-# %%
-# Create Thumbnail for Gallery:
-fig.write_image("../docs/gallery/_thumb_plotly_plotting.png")
+# Create Thumbnail for the Sphinx-Gallery:
+from pathlib import Path 
+if Path("../docs/gallery").exists():
+    fig.write_image("../docs/gallery/_thumb_plotly_plotting.png")
 # sphinx_gallery_thumbnail_path = 'gallery/_thumb_plotly_plotting.png'
 # %%
